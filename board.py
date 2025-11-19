a610af9abae59b3feeeecd017c1d07eac6f76a3a
Momo-PC <molinecharlesp@gmail.com>
2025-11-19 11:36:12 -0500

    idk

--- board.py
+++ board.py
@@ -4,10 +4,10 @@ from cell import Cell
 
 class Board: 
   def __init__(self, width, height, screen, difficulty): 
-    self.width=width
-    self.height=height
-    self.screen=screen
-    self.difficulty=difficulty
+    self.width = width
+    self.height = height
+    self.screen = screen
+    self.difficulty = difficulty
      
   def draw(self): 
     pass
