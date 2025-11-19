bf4ef52b7ee60651ea72915acf2339b1997aa487
Momo-PC <molinecharlesp@gmail.com>
2025-11-19 00:04:06 -0500

    added self stuff

--- cell.py
+++ cell.py
@@ -1,11 +1,15 @@
 class Cell: 
   def __init__(self, value, row, col, screen):
-    pass
+      self.value = value
+      self.row = row
+      self.col = col
+      self.screen = screen
 
   def set_value(self, value):
-    pass
+      self.value = value
     
   def set_temp(self, value):
-    pass
+      self.value = value
+
   def draw(self):
-    pass
+      pass
