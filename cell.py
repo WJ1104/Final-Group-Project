3928ffdd8aad5e2b3c9424f119244e24d16d29e3
Weedchenska Jeanbaptiste <weedchenskajeanbaptiste4@gmail.com>
2025-11-21 22:18:16 -0500

    Update cell.py

--- cell.py
+++ cell.py
@@ -20,19 +20,19 @@ class Cell:
       x = self.col * cell_size # x position of cell
       y = self.row * cell_size # y position of cell
 
-      if self.selected: #draws a red rectangle outline around cell when selected
+      if self.selected: 
           pygame.draw.rect(self.screen, (255, 0, 0), (x, y, cell_size, cell_size), 3)
 
-      font = pygame.font.Font(None, 40) #font for numbers
+      font = pygame.font.Font(None, 40) 
 
       if self.value != 0:
-          text = font.render(str(self.value), True, (0, 0, 0)) #creates the number in black
+          text = font.render(str(self.value), True, (0, 0, 0)) 
           self.screen.blit(text, (x + cell_size // 2 - text.get_width() // 2,
-                                  y + cell_size // 2 - text.get_height() // 2)) # centers the text
+                                  y + cell_size // 2 - text.get_height() // 2)) 
 
       elif self.temp != 0:
           temp_font = pygame.font.Font(None, 25)
-          text = temp_font.render(str(self.temp), True, (128, 128, 128)) #draws temp number in light gray
-          self.screen.blit(text, (x + 5, y + 5)) # puts the temp number in top left
+          text = temp_font.render(str(self.temp), True, (128, 128, 128)) 
+          self.screen.blit(text, (x + 5, y + 5)) 
 
 
